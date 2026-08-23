6d7e84a2d110e3555a9c511f45c88d618c8d8d95
voucherdb <manuals.storage@gmail.com>
2026-08-24 00:30:37 +0300

    Update login.php

--- login.php
+++ login.php
@@ -237,7 +237,7 @@ try {
     <div id="payment-success-view" style="display: none;">
         <h3 class="success-title">✔ Malipo Yamekamilika!</h3> 
         <p style="color: black; font-weight: 500; margin-bottom: 4px;">
-            Tafadhali bonyeza NAKILI kuhifadhi voucher yako, kisha bonyeza HODI kuperuzi mtandaoni. 
+            Bonyeza NAKILI kukupeleka kwenye Mtandao wa TANConnect Wi-Fi.
         </p>
         <div class="voucher-box" id="voucherCode">--------</div>
         <button class="btn" style="margin-bottom: 10px; background: #2ecc71;" onclick="copyVoucher()">NAKILI</button>
